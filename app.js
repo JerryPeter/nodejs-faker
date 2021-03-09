e565bc86377b24feec7742ca4cea3a1549486cc6
JerryPeter <jerry.peter@gmail.com>
2021-03-09 22:46:13 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const faker = require('faker');
+
+faker.locale = "id_ID";
+var _name = faker.name.findName();
+var _email = faker.internet.email();
+var _card = faker.helpers.createCard();
+
+
+console.log("Name : " + _name);
+console.log("Email : " + _email);
+console.log("Card : " + _card);
+
